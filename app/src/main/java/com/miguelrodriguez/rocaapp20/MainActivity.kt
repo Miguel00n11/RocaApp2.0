71ab11301bf69e940c8599433e6eb5b2378a7fc2
Jesus Miguel Rodr�guez Ortega <rooj_civ@hotmail.com>
2023-12-18 14:58:14 -0600

    miguel1eeedfaa

--- app/src/main/java/com/miguelrodriguez/rocaapp20/MainActivity.kt
+++ app/src/main/java/com/miguelrodriguez/rocaapp20/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.miguelrodriguez.rocaapp20
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,17 +18,15 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnAcceder:Button
-    private lateinit var NombreUsuario:EditText
-    private lateinit var Password:EditText
+    private lateinit var btnAcceder: Button
+    private lateinit var NombreUsuario: EditText
+    private lateinit var Password: EditText
     private lateinit var auth: FirebaseAuth
 
 
-
-   companion object{
-       var NombreUsuarioCompanion="NombreUsuario"
-   }
-
+    companion object {
+        var NombreUsuarioCompanion = "NombreUsuario"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +45,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun initUI() {
 
-        btnAcceder.setOnClickListener {  acceder1(NombreUsuario.text.toString(),Password.text.toString()) }
+        btnAcceder.setOnClickListener {
+            acceder1(
+                NombreUsuario.text.toString(),
+                Password.text.toString()
+            )
+        }
     }
 
 //    private fun abrirCalculo_Compactacion(NombreUsuario:String) {
@@ -55,60 +60,74 @@ class MainActivity : AppCompatActivity() {
 //    }
 
     private fun abrirCalculo_Compactacion() {
-        val intent=Intent(this,SeleccionarActividad::class.java )
-        NombreUsuarioCompanion=NombreUsuario.text.toString()
+        val intent = Intent(this, SeleccionarActividad::class.java)
+        NombreUsuarioCompanion = NombreUsuario.text.toString()
 //        intent.putExtra(NombreUsuarioCompanion,NombreUsuario)
         startActivity(intent)
     }
 
     private fun initComponent() {
-        btnAcceder=findViewById(R.id.btnAcceder)
-        NombreUsuario=findViewById(R.id.etEmail)
-        Password=findViewById(R.id.etPassword)
+        btnAcceder = findViewById(R.id.btnAcceder)
+        NombreUsuario = findViewById(R.id.etEmail)
+        Password = findViewById(R.id.etPassword)
 
     }
-    private fun acceder1(email:String,password:String){
-        try {
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("TAG", "createUserWithEmail:success")
-                        consultar_datos.usuarioApp=email
-                        val user = auth.currentUser
-                        NombreUsuarioCompanion=NombreUsuario.text.toString()
-                        Acceder()
+
+    private fun acceder1(email: String, password: String) {
+        if (isNetworkAvailable()) {
+            try {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success")
+                            consultar_datos.usuarioApp = email
+                            val user = auth.currentUser
+                            NombreUsuarioCompanion = NombreUsuario.text.toString()
+                            Acceder()
 //                    updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("TAG", "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        showAlert()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            showAlert()
 //                    updateUI(null)
+                        }
                     }
-                }
-        }catch (e: Exception){}
-
-
+            } catch (e: Exception) {
+            }
+        } else {
+            // Muestra un mensaje indicando que no hay conexión a Internet
+            Toast.makeText(this, "No hay conexión a Internet", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
-    private fun Acceder(){
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
 
-        val Acceder=Intent(this,SeleccionarActividad::class.java)
-        consultar_datos.modoInvitado=false
+    private fun Acceder() {
+
+        val Acceder = Intent(this, SeleccionarActividad::class.java)
+        consultar_datos.modoInvitado = false
 //        Registrarse.putExtra(TAG,"K")
 //            putExtra("Provider",provider.name)
         startActivity(Acceder)
 
     }
-    private fun showAlert(){
-        val builder= AlertDialog.Builder(this)
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
         builder.setTitle("Error de autentificación")
         builder.setMessage("Favor de ingresar la contraseña coreccta.")
-        builder.setPositiveButton("Aceptar",null)
-        val dialog: AlertDialog =builder.create()
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
         dialog.show()
 
 
